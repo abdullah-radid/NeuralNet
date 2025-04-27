94f50623553006ecd5a4634f607d0c4fb043c18e
khalid12321465 <kedris1@students.towson.edu>
2025-04-27 10:12:42 -0400

    Delete generateUsername.js

--- generateUsername.js
+++
@@ -1,6 +0,0 @@
-function generateRandomUsername() {
-    const randomNumber = Math.floor(10000 + Math.random() * 90000); 
-    return `User${randomNumber}`; // user + random 5-digit number
-}
-
-module.exports = generateRandomUsername;
